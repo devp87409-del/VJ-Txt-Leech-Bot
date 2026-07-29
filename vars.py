566e2c9040f3d4184e7b01f273cb6b9995b627a8
devp87409-del <devp87409@gmail.com>
2026-07-29 23:01:24 +0530

    Reformat comments and code structure in vars.py

--- vars.py
+++ vars.py
@@ -6,5 +6,5 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")839
 
